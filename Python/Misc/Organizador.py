b6de6e7059b9a1362ef40f403b35a185ebda6f76
HenriqueTavaresAguiar <ricktavares76@gmail.com>
2020-04-15 15:59:26 -0300

    organizador de arquivos

---
+++ Python/Misc/Organizador.py
@@ -0,0 +1,40 @@
+from os import walk, mkdir, path
+from shutil import move
+
+class Linguagem:
+
+	__slots__ = ("nome", "extensao")
+
+	def __init__(self, nome, *extensao):
+		self.nome = nome
+		self.extensao = extensao
+
+	def organize(self, sourcePath = './'):
+
+		folder = path.join(sourcePath, self.nome)
+
+		try:
+			mkdir(folder)
+
+		except Exception:
+			print(f'\nPasta {self.nome} já existe!\n')
+			exit()
+
+		else:
+			print(f'\nPasta {self.nome} criada!\n')
+
+		for root, dirs, files in walk(sourcePath):
+
+			if root == folder:
+				continue
+
+			for file in files:
+				for ext in self.extensao:
+					if file.endswith(ext):
+						print(f'Movendo {path.join(root, file)} para {path.join(folder, file)} ...', end=' ')
+						move(path.join(root, file), path.join(folder, file))
+						print('pronto')
+
+
+cpp = Linguagem("C++", '.cpp', '.h')
+python.organize('./C++/algoritimos')
