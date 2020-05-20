cb9c4746a7835571a3c56422bd11c72bc7a15d02
HenriqueTavaresAguiar <ricktavares76@gmail.com>
2020-05-20 14:22:49 -0300

    triangle part 2

--- Ruby/koans/triangle.rb
+++ Ruby/koans/triangle.rb
@@ -14,12 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+  raise TriangleError unless triangle_valid?(a, b, c)
+
   if a == b && b == c
     :equilateral
   elsif a != b && b != c && c != a
     :scalene
   else
     :isosceles
+  end
+end
+
+def triangle_valid?(a, b, c)
+  if (a**2 + b**2 + c**2)**2 > 2 * (a**4 + b**4 + c**4)
+    true unless a < 0 || b < 0 || c < 0
+  else
+    false
   end
 end
 
