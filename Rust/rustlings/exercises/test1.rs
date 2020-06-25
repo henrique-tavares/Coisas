a98cbff3f67ed2d052135dd07e36dfcb07748ce5
henrique-tavares <ricktavares76@gmail.com>
2020-06-25 18:04:33 -0300

    test1 feito

--- Rust/rustlings/exercises/test1.rs
+++ Rust/rustlings/exercises/test1.rs
@@ -7,10 +7,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(qtd_apple: i32) -> i32 {
+    if qtd_apple <= 40 {
+        qtd_apple * 2
+    } else {
+        qtd_apple
+    }
+}
 
 // Don't modify this function!
 #[test]
