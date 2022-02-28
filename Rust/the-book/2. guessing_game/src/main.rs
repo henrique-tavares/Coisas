657ad6be4c5760a8406c812aa7cc0f25c1244de8
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2022-02-28 17:09:30 -0300

    2. guessing game

---
+++ Rust/the-book/2. guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use colored::Colorize;
+use rand::Rng;
+
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess:");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess = match guess.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("PLease type a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }
+}
