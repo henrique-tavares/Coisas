d7f54dfdd7efd815ad9687cf1bab8729c3921bef
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2022-02-28 17:23:21 -0300

    update 2.

--- Rust/the-book/2. guessing_game/src/main.rs
+++ Rust/the-book/2. guessing_game/src/main.rs
@@ -6,7 +6,7 @@ use std::{cmp::Ordering, io};
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
         println!("Please input your guess:");
